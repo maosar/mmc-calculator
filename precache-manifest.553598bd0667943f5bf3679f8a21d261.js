6f775dbeecf2c44d4f73d31bd1d9fa33931e49cf
maosar <oerra90@gmail.com>
2019-05-21 07:21:54 +0200

    Updates

---
+++ precache-manifest.553598bd0667943f5bf3679f8a21d261.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056e8585103fd999209fe932243e926b",
+    "url": "/mmc-calculator/index.html"
+  },
+  {
+    "revision": "17abad19628bd14ad71f",
+    "url": "/mmc-calculator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9b21df13d227a333290b",
+    "url": "/mmc-calculator/static/js/2.04c0cb41.chunk.js"
+  },
+  {
+    "revision": "17abad19628bd14ad71f",
+    "url": "/mmc-calculator/static/js/main.8fc41832.chunk.js"
+  },
+  {
+    "revision": "dffb1f23ba6459fd8fee",
+    "url": "/mmc-calculator/static/js/runtime~main.9bb5e160.js"
+  }
+]);
